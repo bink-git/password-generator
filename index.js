f80cd79fc61f276742f2c814055d16c2ff2d1eb7
BINK <bink@email.ua>
2022-10-03 12:44:49 +0300

    final

--- index.js
+++ index.js
@@ -1,168 +1,192 @@
 const characters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "~",
-  "`",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "=",
-  "{",
-  "[",
-  "}",
-  "]",
-  ",",
-  "|",
-  ":",
-  ";",
-  "<",
-  ">",
-  ".",
-  "?",
-  "/",
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '~',
+  '`',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '-',
+  '+',
+  '=',
+  '{',
+  '[',
+  '}',
+  ']',
+  ',',
+  '|',
+  ':',
+  ';',
+  '<',
+  '>',
+  '.',
+  '?',
+  '/',
 ];
 
-let passLeft = document.querySelector(".password-left");
-let passRight = document.querySelector(".password-right");
-let btn = document.querySelector(".btn");
-let num = document.querySelector("input");
-const numCheck = document.getElementById("numCheck");
-const symCheck = document.getElementById("symCheck");
+let passLeft = document.querySelector('.password-left');
+let passRight = document.querySelector('.password-right');
+
+let btn = document.querySelector('.btn');
+let num = document.querySelector('input');
+
+const numCheck = document.getElementById('numCheck');
+const symCheck = document.getElementById('symCheck');
+
+const circleNum = document.querySelector('.circle-num');
+const tumblerNum = document.querySelector('.tumbler-num');
+
+const circleSym = document.querySelector('.circle-sym');
+const tumblerSym = document.querySelector('.tumbler-sym');
+
 let setNum = false;
 let setSym = false;
 
-let input = document.querySelector("input");
-const text = document.getElementById("text");
-// const togg = document.querySelector(".toggle");
-const circle = document.querySelector(".circle");
+let input = document.querySelector('input');
+const text = document.getElementById('text');
 
-numCheck.addEventListener("click", () => {
-  circle.classList.toggle("active");
-  circle.classList.toggle("circle");
-  if (circle.classList.contains("active")) {
+numCheck.addEventListener('click', () => {
+  circleNum.classList.toggle('circle-active');
+  circleNum.classList.toggle('circle');
+  tumblerNum.classList.toggle('tumbler');
+  tumblerNum.classList.toggle('tumbler-active');
+  if (circleNum.classList.contains('circle-active')) {
     setNum = true;
-    console.log("setnum");
+  }
+  if (circleNum.classList.contains('circle')) {
+    setNum = false;
   }
 });
 
-symCheck.addEventListener("click", (e) => {
-  e.target.value = circle.classList.toggle("active");
-  e.target.value = circle.classList.toggle("circle");
-  if (circle.classList.contains("active")) {
+symCheck.addEventListener('click', () => {
+  circleSym.classList.toggle('circle-active');
+  circleSym.classList.toggle('circle');
+  tumblerSym.classList.toggle('tumbler');
+  tumblerSym.classList.toggle('tumbler-active');
+  if (circleSym.classList.contains('circle-active')) {
     setSym = true;
-    console.log("setsym", e);
+  }
+  if (circleSym.classList.contains('circle')) {
+    setSym = false;
   }
 });
 
-let lehgth;
-input.addEventListener("input", (e) => {
-  length = e.target.value;
+let passLehgth;
+input.addEventListener('input', (e) => {
+  passLength = e.target.value;
 });
+
+const passWithLetters = characters.slice(0, 52);
+console.log(passWithLetters);
+
+const passWithNumbers = characters.slice(0, 62);
+console.log(passWithNumbers);
 
 function generate() {
   let pass1 = [];
   let pass2 = [];
 
-  for (let i = 0; i < length; i++) {
-    if (setNum) {
+  for (let i = 0; i < passLength; i++) {
+    if (!setNum && !setSym) {
       pass1.push(
-        characters.slice(54, 92)[
-          Math.floor(Math.random() * characters.slice(54, 92).length)
-        ]
+        passWithLetters[Math.floor(Math.random() * passWithLetters.length)]
       );
       pass2.push(
-        characters.slice(54, 92)[
-          Math.floor(Math.random() * characters.slice(54, 92).length)
-        ]
+        passWithLetters[Math.floor(Math.random() * passWithLetters.length)]
       );
-    } else {
+    } else if (setNum && !setSym) {
+      pass1.push(
+        passWithNumbers[Math.floor(Math.random() * passWithNumbers.length)]
+      );
+      pass2.push(
+        passWithNumbers[Math.floor(Math.random() * passWithNumbers.length)]
+      );
+    } else if (setNum && setSym) {
       pass1.push(characters[Math.floor(Math.random() * characters.length)]);
       pass2.push(characters[Math.floor(Math.random() * characters.length)]);
     }
   }
 
-  passLeft.textContent = pass1.join("");
-  passRight.textContent = pass2.join("");
+  passLeft.textContent = pass1.join('');
+  passRight.textContent = pass2.join('');
 }
 
-btn.addEventListener("click", generate);
+btn.addEventListener('click', generate);
 
-passLeft.addEventListener("click", (e) => {
+passLeft.addEventListener('click', (e) => {
   const content = passLeft.textContent;
   navigator.clipboard.writeText(content);
 });
 
-passRight.addEventListener("click", (e) => {
+passRight.addEventListener('click', (e) => {
   const content = passRight.textContent;
   navigator.clipboard.writeText(content);
 });
